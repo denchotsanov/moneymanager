7ca6997ee0bd524bba7197ce3bd3f97e1ed579d7
Dencho Tsanov <tsanov.dencho@gmail.com>
2022-08-20 14:26:46 +0300

    add currency page in menu

--- src/components/SlideMenu.js
+++ src/components/SlideMenu.js
@@ -6,6 +6,8 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
 import AccountBalanceRoundedIcon from '@mui/icons-material/AccountBalanceRounded';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import LocalAtmIcon from '@mui/icons-material/LocalAtm';
 import { styled, useTheme } from '@mui/material/styles';
 
 const drawerWidth = 240;
@@ -58,7 +60,7 @@ export const SlideMenu = ({
                     <ListItem disablePadding>
                         <ListItemButton>
                             <ListItemIcon>
-                                <AccountBalanceRoundedIcon/>
+                                <CreditCardIcon/>
                             </ListItemIcon>
                             <ListItemText primary="Transaction"/>
                         </ListItemButton>
@@ -71,6 +73,16 @@ export const SlideMenu = ({
                                 <AccountBalanceRoundedIcon/>
                             </ListItemIcon>
                             <ListItemText primary="Accounts"/>
+                        </ListItemButton>
+                    </ListItem>
+                </Link >
+                <Link to="/currency">
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <LocalAtmIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary="Currency"/>
                         </ListItemButton>
                     </ListItem>
                 </Link >
